4eeaa1d517d88be311bc5cca86e7bacde9d5e7a3
Brayam Ayala <b.ayalaga06@gmail.com>
2025-04-27 15:54:00 -0500

    Implementación de los endpoints de las entidades.

---
+++ src/main/java/com/bag/gestion_biblioteca/controller/AuthorController.java
@@ -0,0 +1,55 @@
+package com.bag.gestion_biblioteca.controller;
+
+import com.bag.gestion_biblioteca.model.dto.AuthorResponse;
+import com.bag.gestion_biblioteca.model.dto.CreateAuthorRequest;
+import com.bag.gestion_biblioteca.service.AuthorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/authors")
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    @PostMapping
+    public ResponseEntity<AuthorResponse> save(
+            @Valid
+            @RequestBody CreateAuthorRequest request) {
+
+        AuthorResponse author = authorService.save(request);
+        return ResponseEntity.created(URI.create("api/auhtors/" + author.getId()))
+                .body(author);
+    }
+
+    @GetMapping
+    public List<AuthorResponse> findAll() {
+        return authorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public AuthorResponse findById(@PathVariable Long id) {
+        return authorService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public AuthorResponse update(@PathVariable Long id,
+                                 @Valid
+                                 @RequestBody CreateAuthorRequest request) {
+
+        return authorService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        authorService.deleteById(id);
+    }
+}
+
+
