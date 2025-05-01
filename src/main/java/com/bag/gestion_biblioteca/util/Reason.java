c9f96ce0bc3c4aa576f28129ac7b253476acc243
Brayam Ayala <b.ayalaga06@gmail.com>
2025-05-01 12:32:51 -0500

    Agregar nuevos enums: Reason, Type y Status para gestionar estados y razones en la biblioteca

---
+++ src/main/java/com/bag/gestion_biblioteca/util/Reason.java
@@ -0,0 +1,8 @@
+package com.bag.gestion_biblioteca.util;
+
+public enum Reason {
+    RETURN_DELAY,
+    LOST_BOOK,
+    DAMAGED_BOOK,
+    OTHER
+}
--- src/main/java/com/bag/gestion_biblioteca/util/Type.java
+++ src/main/java/com/bag/gestion_biblioteca/util/Type.java
@@ -1,4 +1,7 @@
 package com.bag.gestion_biblioteca.util;
 
 public enum Type {
+    DAMAGED,
+    LOST,
+    OTHER
 }
