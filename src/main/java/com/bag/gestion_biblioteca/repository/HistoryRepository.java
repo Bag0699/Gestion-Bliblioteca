4db848a20dfb3dbefc34da8bc72ecb0e44950717
Brayam Ayala <b.ayalaga06@gmail.com>
2025-05-01 17:46:44 -0500

    Eliminados servicios y controladores relacionados con historial y devoluciones

--- src/main/java/com/bag/gestion_biblioteca/repository/HistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bag.gestion_biblioteca.repository;
-
-import com.bag.gestion_biblioteca.model.entity.History;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HistoryRepository extends JpaRepository<History, Long> {
-}
